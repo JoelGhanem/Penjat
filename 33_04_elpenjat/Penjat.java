73bb7b72435386af9a8127cf7b82f5ab7d376d86
JoelGhanem <113618468+JoelGhanem@users.noreply.github.com>
2024-06-27 22:35:16 +0200

    Update Penjat.java

--- 33_04_elpenjat/Penjat.java
+++ 33_04_elpenjat/Penjat.java
@@ -26,24 +26,16 @@ public class Penjat {
             } else {
                 utilitzades = lletresUtilitzades(utilitzades, letra);
             }
-            //Función que imprime los intentos disponibles
+            /Función que imprime los intentos disponibles
             intents = intentsDisponibles(intents);
             //Función que pide la letra, mira si es prou o no y la devuelve para que siga en juego
-            letra = introdueixLletra();
+            letra = introdueixLletra()
             if (letra == 'ñ') {
                 prou(jugadas, ganadas, perdidas, canceladas);
             }
             break;
         }
     }
-    // hay que hacer 2 strings para que se vaya mostrando la palabrta con y sion censura
-    public static int paraulaEnJocJoc(int numero, String paraulaEnJoc, char letra) {
-        boolean encertat = false;
-        String oculto = ""; 
-        String muestra = "";
-        for (int i = 0; i < paraulaEnJoc.length(); i++) {
-            oculto = oculto + "*";
-        }
         System.out.println("Paraula: " + oculto);
         oculto = "";
         numero = numero + 1;
@@ -53,9 +45,22 @@ public class Penjat {
                     oculto = oculto +  letra;
                 } else { 
                     oculto = oculto + "*";
+                        throw new CustomException("Swiper no copies, el gat Renat no te lo perdonaria nunca");
                 }
             }
         }
+  } catch (CustomException e) {
+            System.out.println(e.getMessage());
+        }
+    // hay que hacer 2 strings para que se vaya mostrando la palabrta con y sion censura
+    public static int paraulaEnJocJoc(int numero, String paraulaEnJoc, char letra) {
+        boolean encertat = false;
+        String oculto = ""; 
+        String muestra = "";
+        for (int i = 0; i < paraulaEnJoc.length(); i++) {
+            oculto = oculto + "*";
+        }
+    
         return numero;
     }
     public static String lletresUtilitzades(String utilitzades, char letra) {
@@ -84,8 +89,8 @@ public class Penjat {
         return utilitzades;
     }
     public static int intentsDisponibles(int intents) {
-        int tiradas = intents;
-        tiradas -=1;
+        int tiradas = intent;
+        tiradas +=1;
         System.out.println("Intents disponibles: " + tiradas);
         return tiradas;
     }
@@ -123,14 +128,17 @@ public class Penjat {
         String finalitza = "";
         System.out.println("Vols finalitzar?");
         finalitza = Entrada.readLine();
-        if (finalitza.equals("sí")) {
-            System.out.println("Paraules jugades: " + jugadas);
-            System.out.println("Paraules encertades: " + ganadas);
-            System.out.println("Paraules fallades: " + perdidas);
-            System.out.println("Paraules cancel·lades: " + canceladas);
+        if (finalitza.equals("si")) {
+            System.out.println("Paraules jugades: " + jugades);
+            System.out.println("Paraules encertades: " + ganades);
+            System.out.println("Paraules fallades: " + perdides);
+            System.out.println("Paraules cancel·lades: " + cancelades);
             System.out.println("Espero que t'hagis divertit");
             prou = true;
         }
-        return prou ;
+        return prou 
+            
+            ;
     }
 }
+//El Moisés sabe que te estas copiando bobo, ponte a trabajar vago
